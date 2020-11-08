2e7a0021869e4be3e51e96d96fd0501ef50cc38c
Fabio Galante Mans <fabiogalantemans@gmail.com>
2020-11-08 18:47:42 -0300

    Controller

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,12 +1,15 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CommandAPI.Controllers {
+namespace CommandAPI.Controllers
+{
     [Route ("api/[controller]")]
     [ApiController]
-    public class CommandsController : ControllerBase {
+    public class CommandsController : ControllerBase 
+    {
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get () {
+        public ActionResult<IEnumerable<string>> Get () 
+        {
             return new string[] { "this", "is", "hard", "coded" };
         }
     }
